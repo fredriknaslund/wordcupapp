b91e94c19a5ff5561599b7b4c917b5a7ff0a0b13
fnaslund <fredrik.naslund@creuna.se>
2018-06-19 09:52:34 +0200

    Add webpack dev server.

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>My React App</h1>
+                <h1>My React App is working</h1>
             </div>
         );
     }
